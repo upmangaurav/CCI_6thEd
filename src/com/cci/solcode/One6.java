4eff09883ff23f7d736a32a7e707240279b6f1d2
upmangaurav <upmangaurav@gmail.com>
2018-07-29 23:21:50 -0400

    regulation push

---
+++ src/com/cci/solcode/One6.java
@@ -0,0 +1,48 @@
+package com.cci.solcode;
+
+import java.util.Scanner;
+
+public class One6 {
+
+    private static String strCompress(String str){
+        int count = 0;
+        int grossCount = 0;
+        char ch = str.charAt(0);
+
+        for (int i = 1; i < str.length(); i++){
+            if(str.charAt(i) != ch){
+                grossCount += count - 1;
+                count = 0;
+                ch = str.charAt(i);
+            }
+            else
+                count ++;
+        }
+        grossCount += count - 1;
+        if(grossCount <= 0) return str;
+
+        count = 0;
+        ch = str.charAt(0);
+        String finalStr = "";
+
+        for (int i = 1; i < str.length(); i++){
+            if(str.charAt(i) != ch){
+                finalStr = finalStr + ch + (count + 1);
+
+                count = 0;
+                ch = str.charAt(i);
+            }
+            else
+                count ++;
+        }
+        finalStr = finalStr + ch + (count + 1);
+        return finalStr;
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(strCompress(str));
+    }
+}
