fa12c94686288c5ba04fca63653b7121cf44b094
upmangaurav <upmangaurav@gmail.com>
2018-07-30 15:43:52 -0400

    regulation push

---
+++ src/com/cci/solcode/One9.java
@@ -0,0 +1,26 @@
+package com.cci.solcode;
+
+import java.util.Scanner;
+
+public class One9 {
+    protected static boolean isSubString(String bigger, String smaller){
+        if(bigger == null || smaller == null)	return false;
+        if((bigger.indexOf(smaller)) > 0)	return true;
+        else return false;
+
+    }
+    protected static boolean isRotation(String str1, String str2){
+        if(str1.length() != str2.length()) return false;
+        return isSubString(str1 + str1, str2);
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+
+        System.out.println(isRotation(str1, str2));
+
+    }
+}
