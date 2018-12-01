e9770f06005952a40a46dcded83b135eca8b3a09
upmangaurav <upmangaurav@gmail.com>
2018-12-01 00:08:41 -0500

    seasonal

---
+++ src/Random.java
@@ -0,0 +1,24 @@
+public class Random {
+    public static void main(String[] Args){
+        System.out.println("whoa");
+
+        int p = 102345;
+        int[] pa = new int[p];
+        int q = -p;
+        String st = Integer.toString(q);
+//        String st2= p;
+        System.out.println(-7 / 10);
+
+        String str = " " + q;
+
+        System.out.println(st);
+
+
+        int[] arr1 = {1,2,3};
+        for(int i = 0; i < arr1.length; i++){
+            System.out.println(i);
+        }
+
+        System.out.println(q/10);
+    }
+}
